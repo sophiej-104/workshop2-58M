5dfcce30b3037bc5a5f9460afde7a0c90bc560f9
Sophie Davies <sophiejeandavies@gmail.com>
2020-10-30 11:52:22 +0000

    Plotting summarised data using pipe function

--- analysis.R
+++ analysis.R
@@ -22,3 +22,47 @@ hdi_summary <- hdi_no_na %>%
             n = length(HDI_Value),
             sd_index = sd(HDI_Value),
                           se_index = sd(HDI_Value)/sqrt(n()))
+
+# Filter the summary to get just the ten countries with the lowest mean HDI using:
+
+hdi_summary_low <- hdi_summary %>% 
+  filter(rank(mean_index) < 11)
+
+hdi_summary_low
+
+# Plotting
+
+hdi_summary_low %>% 
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_index - se_index,
+                    ymax = mean_index + se_index)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI Value") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
+
+hdi_tidy %>% 
+  group_by(Country) %>% 
+  summarise(mean_index = mean(HDI_Value),
+            n = length(HDI_Value),
+            sd_index = sd(HDI_Value),
+            se_index = sd(HDI_Value)/sqrt(n())) %>%
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_index - se_index,
+                    ymax = mean_index + se_index)) +
+  scale_y_continuous(expand = c(0, 0),
+                     name = "HDI Value") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "Countries",
+                   guide = guide_axis(check.overlap = T)) +
+  theme_classic() +
+  coord_flip()
